c09ef375f6691e7e4378552fd9e9272cd45f9495
Rajak13 <queresma05@gmail.com>
2026-01-15 04:45:07 +0545

    chore: remove sensitive and unnecessary files from tracking

--- migrations/20241210000001_add_tags_to_tasks.sql
+++
@@ -1,5 +0,0 @@
--- Add tags column to tasks table
-ALTER TABLE tasks ADD COLUMN IF NOT EXISTS tags TEXT[];
-
--- Create index for tags array
-CREATE INDEX IF NOT EXISTS idx_tasks_tags ON tasks USING gin(tags);
